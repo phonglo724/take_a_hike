049e74b42d4f3383858a20225f5001b95f081f87
Phong Lo <phonglo724@gmail.com>
2021-02-17 16:28:37 -0700

    collab 2

--- runner.rb
+++ runner.rb
@@ -1,8 +1,26 @@
 require_relative 'config/environment'
+$prompt = TTY::Prompt.new
 
+def welcome 
+    title = Artii::Base.new(:font => "slant")
+    puts title.asciify("Take a Hike!").green
+end
 
+def intro_menu
+    intro_option = $prompt.select("What would you like to do?", ["Take a hike", "Find a hiker"])
+    if(intro_option == "Take a hike")
+        hiker_name = $prompt.ask("Name:")
+        Hiker.create(name: hiker_name)
+        puts "Welcome #{hiker_name}!"
+    else
+        exit!
+    end
+end
 
-binding.pry
+welcome
+intro_menu
+
+#binding.pry
 
 
 
