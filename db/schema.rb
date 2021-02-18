e0741c73e063897b158fe634969f071f637db662
Phong Lo <phonglo724@gmail.com>
2021-02-17 18:42:02 -0700

    collab 3

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_17_183052) do
+ActiveRecord::Schema.define(version: 2021_02_18_013536) do
 
   create_table "hikers", force: :cascade do |t|
     t.string "name"
